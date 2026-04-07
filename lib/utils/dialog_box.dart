68b82ca14e50ccd18a0598b575e8486a28816c40
Prashant Timalsina <prashanttimalsina2@gmail.com>
2026-04-08 00:19:15 +0545

    A new dialog box and 2 new buttons in it

---
+++ lib/utils/dialog_box.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/utils/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  const DialogBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return AlertDialog(
+      backgroundColor: theme.colorScheme.primaryContainer,
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new Task",
+              ),
+            ),
+
+            //buttons -> Save + Cancel
+            Row(
+              children: [
+                //Save
+                MyButton(onPressed: () {}, text: "Save"),
+
+                //Cancel
+                MyButton(onPressed: () {}, text: "Cancel"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
